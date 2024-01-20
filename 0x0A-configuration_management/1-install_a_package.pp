d1590cb9864bfad8142c9c18f18a5de0f058082f
khaoulab <kholie.be@gmail.com>
2024-01-20 09:22:32 -0800

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Puppet manifest using the pip provider to install Flask 2.1.0 and ensure Werkzeug 2.1.1 is installed
+# Install Flask using the pip provider
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Ensure Werkzeug is installed using the pip provider
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
